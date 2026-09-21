84b55858486473268f62490c566b2d195f850869
Projects Coordinator <coordinator@projects.local>
2026-09-21 13:30:21 +0000

    test: Update Jest configuration for frontend testing

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   testMatch: ['**/tests/**/*.test.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/tests/__mocks__/styleMock.js',
   },
   collectCoverageFrom: [
     'src/**/*.{js,jsx}',
     '!src/index.js',
+    '!src/index.jsx',
   ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
